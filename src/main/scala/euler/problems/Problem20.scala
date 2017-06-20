f8f7ed6105314037a72c1274fa649f24de4bf896
gnavalesi <guidonavalesi@gmail.com>
2017-06-20 16:59:21 -0300

    Fix

--- src/main/scala/euler/problems/Problem20.scala
+++ src/main/scala/euler/problems/Problem20.scala
@@ -6,5 +6,5 @@ import euler.utils.{AdvancedMath, Problem}
   * @author guido
   */
 object Problem20 extends Problem {
-  override def solution(): Any = AdvancedMath.factorial(100).toString().split("").map(_.toInt).sum
+  override def solution(): Any = AdvancedMath.factorial(BigInt(100)).toString().split("").map(_.toInt).sum
 }
