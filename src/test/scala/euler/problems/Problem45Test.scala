acc300723d968ac100e1ddff148a65448bec2bcd
gnavalesi <guidonavalesi@gmail.com>
2017-09-02 01:54:18 -0300

    Problem 45

---
+++ src/test/scala/euler/problems/Problem45Test.scala
@@ -0,0 +1,12 @@
+package euler.problems
+
+import org.scalatest.FlatSpec
+
+/**
+  * @author guido
+  */
+class Problem45Test extends FlatSpec {
+  it should "return the correct answer" in {
+    assert(Problem45.solution() == 1533776805)
+  }
+}
