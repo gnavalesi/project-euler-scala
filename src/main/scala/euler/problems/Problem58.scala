2e41352ed70de119ce90c031362f8a9a6f9faaa7
gnavalesi <guidonavalesi@gmail.com>
2017-12-28 20:09:30 -0300

    Problem 58

--- src/main/scala/euler/problems/Problem58.scala
+++ src/main/scala/euler/problems/Problem58.scala
@@ -1,18 +1,18 @@
 package euler.problems
 
-import euler.utils.Problem
+import euler.utils.{Primes, Problem}
 
 /**
   * Problem 58
   *
   * Starting with 1 and spiralling anticlockwise in the following way, a square spiral with side length 7 is formed.
-  *   (37)  36   35   34   33   32  (31)
-  *    38  (17)  16   15   14  (13)  30
-  *    39   18  ( 5)   4  ( 3)  12   29
-  *    40   19    6    1    2   11   28
-  *    41   20  ( 7)   8    9   10   27
-  *    42   21   22   23   24   25   26
-  *   (43)  44   45   46   47   48   49
+  * (37)  36   35   34   33   32  (31)
+  * 38  (17)  16   15   14  (13)  30
+  * 39   18  ( 5)   4  ( 3)  12   29
+  * 40   19    6    1    2   11   28
+  * 41   20  ( 7)   8    9   10   27
+  * 42   21   22   23   24   25   26
+  * (43)  44   45   46   47   48   49
   *
   * It is interesting to note that the odd squares lie along the bottom right diagonal, but what is more interesting is
   * that 8 out of the 13 numbers lying along both diagonals are prime; that is, a ratio of 8/13 ≈ 62%.
@@ -24,23 +24,29 @@ import euler.utils.Problem
   **/
 object Problem58 extends Problem with App {
 
-  def expansionsStream(): Stream[(BigInt, BigInt)] = (BigInt(3), BigInt(2)) #:: expansionsStream(1, 2)
+  case class Square(sideLength: Int, corners: (Int, Int, Int, Int))
 
-  def expansionsStream(previousNumerator: BigInt, previousDenominator: BigInt): Stream[(BigInt, BigInt)] = {
-    val numerator = previousDenominator
-    val denominator = previousNumerator + 2 * previousDenominator
+  def squaresStream(): Stream[Square] = squaresStream(Square(1, (1, 1, 1, 1)))
 
-    (denominator + numerator, denominator) #:: expansionsStream(numerator, denominator)
+  def squaresStream(square: Square): Stream[Square] = {
+    val newURCorner = square.corners._4 + square.sideLength + 1
+    val newULCorner = newURCorner + square.sideLength + 1
+    val newDLCorner = newULCorner + square.sideLength + 1
+    val newDRCorner = newDLCorner + square.sideLength + 1
+    val value = Square(square.sideLength + 2, (newURCorner, newULCorner, newDLCorner, newDRCorner))
+
+    value #:: squaresStream(value)
   }
 
-  def toDigitsSize(a: BigInt): BigInt = a.toString.length
+  def countPrimes(n: Product): Int = n.productIterator.count(n => Primes.int.isPrime(n.asInstanceOf[Int]))
 
-  def toDigitsSize(pair: (BigInt, BigInt)): (BigInt, BigInt) = (toDigitsSize(pair._1), toDigitsSize(pair._2))
+  def findLevel(primesCount: Int, squares: Stream[Square]): Int = {
+    val primes = primesCount + countPrimes(squares.head.corners)
+    if(primes * 1.0 / (squares.head.sideLength * 2 - 1) < 0.1) squares.head.sideLength
+    else findLevel(primes, squares.tail)
+  }
 
-  override def solution(): Any = expansionsStream()
-    .take(1000)
-    .map(toDigitsSize)
-    .count(a => a._1 > a._2)
+  override def solution(): Any = findLevel(0, squaresStream())
 
   println(solution())
 }
---
+++ src/test/scala/euler/problems/Problem58Test.scala
@@ -0,0 +1,13 @@
+package euler.problems
+
+import euler.problems.Problem58._
+import org.scalatest.FlatSpec
+
+/**
+  * @author guido
+  */
+class Problem58Test extends FlatSpec {
+  it should "return the correct answer" in {
+    assert(solution() == 26241)
+  }
+}
